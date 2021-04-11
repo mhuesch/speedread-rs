5c30153efb7021d0141e3b8e4480eaab23fee37b
Michael Hueschen <m@mhueschen.space>
2021-04-11 15:17:08 +0400

    niv/lorri/direnv -ify

--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
-{ pkgs ? import <unstable> {} }:
+let
+  sources = import ./nix/sources.nix;
+  rust = import ./nix/rust.nix { inherit sources; };
+in
+
+{ pkgs ? import sources.nixpkgs {}
+}:
+
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    rustc
-    cargo
-    rustfmt
+    rust
   ];
 }
