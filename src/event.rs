b280b945713c93bb62e783be436cf10f0f80f651
Michael Hueschen <m@mhueschen.space>
2021-10-20 14:30:53 -0600

    switch to an "event driven" style
    this seems to dramatically reduce CPU utilization, as we would expect
    from removing an (unnecessary) busy loop which re-renders the screen
    needlessly.
    
    closes #3.

--- src/event.rs
+++ src/event.rs
@@ -1,22 +1,27 @@
-use std::sync::mpsc;
-use std::thread;
-
+use std::{
+    sync::{mpsc, mpsc::Receiver},
+    thread,
+    time::Duration,
+};
 use termion::event::Key;
 use termion::input::TermRead;
 
 pub enum Event<I> {
     Input(I),
+    Tick,
 }
 
 pub struct Events {
     rx: mpsc::Receiver<Event<Key>>,
     input_handle: thread::JoinHandle<()>,
+    tick_handle: thread::JoinHandle<()>,
 }
 
 impl Events {
-    pub fn new() -> Events {
+    pub fn new(tick_dur_recv: Receiver<Duration>) -> Events {
         let (tx, rx) = mpsc::channel();
         let input_handle = {
+            let tx = tx.clone();
             thread::spawn(move || match termion::get_tty() {
                 Ok(input) => {
                     for key in input.keys().filter_map(|evt| evt.ok()) {
@@ -29,10 +34,31 @@ impl Events {
                 Err(msg) => panic!("{}", msg),
             })
         };
-        Events { rx, input_handle }
+        let tick_handle = {
+            thread::spawn(move || loop {
+                match tick_dur_recv.recv() {
+                    Ok(dur) => {
+                        thread::sleep(dur);
+                        if let Err(err) = tx.send(Event::Tick) {
+                            eprintln!("{}", err);
+                            break;
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("{}", err);
+                        break;
+                    }
+                }
+            })
+        };
+        Events {
+            rx,
+            input_handle,
+            tick_handle,
+        }
     }
 
-    pub fn next(&self) -> Result<Event<Key>, mpsc::TryRecvError> {
-        self.rx.try_recv()
+    pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> {
+        self.rx.recv()
     }
 }
